ceb40a6fee95ca23a262a8fdfa5b74d18df9b33c
prushton2 <peterrushton418@gmail.com>
2025-09-27 17:47:14 -0400

    Added rainbow

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use std::f64::consts;
 use openrgb2::{OpenRgbResult, OpenRgbClient, Color};
 use tokio;
 // use openrgb2::ControllerIndex;
+
+/*
+AMD Wraith Prism
+ASUS TUF GAMING X570-PLUS
+Corsair K95 RGB PLATINUM XT
+Logitech G915 Wireless RGB Mechanical Gaming Keyboard
+*/
+
 
 #[tokio::main]
 async fn main() -> OpenRgbResult<()> {
@@ -12,19 +21,24 @@ async fn main() -> OpenRgbResult<()> {
     for controller in controllers {
         let mut cmd = controller.cmd();
         
-        println!("Controller ID: {}", controller.name());
+        println!("{}", controller.name());
         
         for led in 0..controller.num_leds() {
 
-            let color: Color = Color::new((((led%3==0) as i32)*255) as u8, (((led%3==1) as i32)*255) as u8, (((led%3==2) as i32)*255) as u8);
-            println!("Color {}", color);
+            // let mut color: Color = Color::new((led/4) as u8,0,0);
 
-            match cmd.set_led(led, color) {
+            // if led <= 42 {
+            //     color = Color::new(
+            //         (((led%3==0) as i32)*255) as u8,
+            //         (((led%3==1) as i32)*255) as u8,
+            //         (((led%3==2) as i32)*255) as u8
+            //     );
+            // }
+
+            match cmd.set_led(led, CalculateRainbow((led/8) as f64)) {
                 Ok(_) => {}
                 Err(t) => panic!("{}", t)
             };
-
-            // tokio::time::sleep(std::time::Duration::from_secs(1)).await;
         }
         cmd.execute().await?;
 
@@ -33,4 +47,12 @@ async fn main() -> OpenRgbResult<()> {
     }
 
     Ok(())
+}
+
+fn CalculateRainbow(offset: f64) -> Color {
+    return Color::new(
+        (255.0*((offset                       ).cos())) as u8,
+        (255.0*((offset + (2.0*consts::PI)/3.0).cos())) as u8,
+        (255.0*((offset + (4.0*consts::PI)/3.0).cos())) as u8,
+    )
 }
