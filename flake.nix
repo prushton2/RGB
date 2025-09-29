cdbc660efabeba64e83ca0c207aa5e8282afa6fc
prushton2 <peterrushton418@gmail.com>
2025-09-29 00:21:59 -0400

    did flake (i think)

--- flake.nix
+++ flake.nix
@@ -1,22 +1,32 @@
-with import <nixpkgs> { };
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-rustPlatform.buildRustPackage rec {
-  name = "custom-rgb-${version}";
-  version = "0.1.0";
-  src = ./.;
+  outputs = { self, nixpkgs }@inputs: {
+    defaultPackage = let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in  
+      pkgs.rustPlatform.buildRustPackage rec {
+        name = "custom-rgb-${version}";
+        version = "0.1.0";
+        src = ./.;
 
-  buildInputs = with pkgs; [ 
-    openssl
-    pkg-config
-  ];
+        buildInputs = with pkgs; [ 
+          openssl
+          pkg-config
+        ];
 
-  checkPhase = "";
-  cargoHash = "sha256-aljNMSWxjcUbznEFyuECIWawXy9hfesETzS8XjNWYPo=";
+        checkPhase = "";
+        cargoHash = "sha256-aljNMSWxjcUbznEFyuECIWawXy9hfesETzS8XjNWYPo=";
 
-  meta = {
-    description = "My custom OpenRGB Config";
-    homepage = https://github.com/prushton2/openrgb;
-    license = lib.licenses.mit;
-    platforms = lib.platforms.linux;
+        meta = {
+          description = "My custom OpenRGB Config";
+          homepage = https://github.com/prushton2/openrgb;
+          license = pkgs.lib.licenses.mit;
+          platforms = pkgs.lib.platforms.linux;
+        };
+      };
   };
 }
