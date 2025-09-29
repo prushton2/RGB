60a2edd82c74e229942cc69438ab91888c7529bc
prushton2 <peterrushton418@gmail.com>
2025-09-29 12:46:04 -0400

    fixed packaging

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ lib, rustPlatform, openssl, pkg-config}:
+
+rustPlatform.buildRustPackage rec {
+  name = "customrgb-${version}";
+  version = "0.1.0";
+  src = ./.;
+
+  buildInputs = [ 
+    openssl
+    pkg-config
+  ];
+
+  checkPhase = "";
+  cargoHash = "sha256-aljNMSWxjcUbznEFyuECIWawXy9hfesETzS8XjNWYPo=";
+
+  meta = {
+    description = "My custom OpenRGB Config";
+    homepage = https://github.com/prushton2/openrgb;
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+  };
+}
