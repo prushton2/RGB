3e66afa6f8770b094c4039e060a1ca47cede49a5
prushton2 <peterrushton418@gmail.com>
2025-09-30 11:45:21 -0400

    flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,20 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }: {
+    packages.x86_64-linux = 
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        rustPlatform = pkgs.rustPlatform;
+        openssl = pkgs.openssl;
+        pkg-config = pkgs.pkg-config;
+        myPackage = import ./default.nix { inherit lib rustPlatform openssl pkg-config; };
+      in 
+      {
+        default = myPackage;
+      };
 
+    nixosModules.default = import ./customrgb.nix;
   };
 }
